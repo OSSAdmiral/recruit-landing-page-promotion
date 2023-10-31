1b2443095ccd985e6e8bc90d51288e42ea848bf7
MarJose <18107626+MarJose123@users.noreply.github.com>
2023-10-31 13:53:47 +0800

    change wording

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,7 +28,7 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Recruitment is not about filling vacancies; it's about finding and engaging the right talent who will help shape the future of your organization.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://github.com/OSSAdmiral/Recruit">Download</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://github.com/OSSAdmiral/Recruit">Download for free</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://github.com/OSSAdmiral/Recruit">Learn more</a>
